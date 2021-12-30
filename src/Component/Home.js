cc1871048725ec3ae50fc525088cc9e741147da3
Sidhant Gupta <sidhantgupta1983@gmail.com>
2021-12-30 20:15:43 +0530

    reacthandson4

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,11 +5,14 @@ const Home = () => {
         <div>
             <h2>Welcome to Gurukul</h2>
             
-                <pre><h2>A one of its kind, hi-tech international learning center <br/>
+                <pre className="msgBox"><h2>A one of its kind, hi-tech international learning center <br/>
                     offering different career and lifestyle-oriented activities</h2>
                     <br/>
-                    <h2>That's all your child needs to Learn and Grow</h2>
-                </pre>    
+                    <h2>That's all your child needs to Learn and Grow!!</h2>
+                </pre>
+                <br/>    
+                <br/>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2FGurukullearningcentre%2F&psig=AOvVaw0_BImo7XerbUrPnk4TX7cI&ust=1640961721379000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD3uvXgi_UCFQAAAAAdAAAAABAJ" className="alignImage"/>  
             
         </div>
     )
